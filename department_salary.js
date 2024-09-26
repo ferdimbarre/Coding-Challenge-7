95cdd301f9b5cb4185aeac7bcb9728ea58b5b0d8
Fernanda <fernanda19@usf.edu>
2024-09-26 11:20:24 -0400

    Task 2: Create recursive function to calculate department salary

--- department_salary.js
+++ department_salary.js
@@ -52,3 +52,22 @@ const company = {
     ]
 };
 // this is a "company"bject which stores the information for different employees, organized by departments
+
+// Create a Recursive Function to Calculate Total Salary for a Department
+function calculateDepartmentSalary (departments,nameOfDepartment) {
+let thisDepartment = departments.find(department => department.departmentName === nameOfDepartment); // find the department by the input
+function calculateEmployeeSalary(employee) {
+    let total = employee.salary;
+    employee.subordinates.forEach(subordinate => { //iterate to check if there are subordinates, use recursive call to calculate salaries 
+        total += calculateEmployeeSalary(subordinate); }) ; 
+        return total }
+let sumOfDepartment = thisDepartment.employees.reduce ((total, employee) => { return total += calculateEmployeeSalary(employee);},0) //reduce iterating over array 
+return sumOfDepartment} ; 
+
+// example call
+console.log(calculateDepartmentSalary(company.departments, "Engineering")) //expected output is 330000
+
+
+
+
+
